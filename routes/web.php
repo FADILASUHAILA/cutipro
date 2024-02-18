78ac2ed4f75ba0a01decf42babc279458fee6853
muhammadrival2003 <muhammadrival040303@gmail.com>
2024-02-18 22:45:35 +0700

    Menambhakn route superadmin, admin dan user

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,18 @@ Route::group(['middleware' => ['auth', 'checkrole:1,2,3']], function() {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/redirect', [RedirectController::class, 'cek']);
 });
+
+// untuk superadmin
+Route::group(['middleware' => ['auth', 'checkrole:1']], function() {
+    Route::get('/superadmin', [SuperadminController::class, 'index']);
+});
+
+// untuk admin
+Route::group(['middleware' => ['auth', 'checkrole:2']], function() {
+    Route::get('/admin', [AdminController::class, 'index']);
+});
+
+// untuk pegawai
+Route::group(['middleware' => ['auth', 'checkrole:3']], function() {
+    Route::get('/karyawan', [UserController::class, 'index']);
+});
